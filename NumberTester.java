cee84c8cdd63bc81ca2f710d8315fb8fef1e78aa
LukasStranzes <lukas.stranzinger@outlook.at>
2020-09-29 12:55:38 +0200

    lambda funktion den settern PrimeTester und PalindromTester mitgegeben

--- NumberTester.java
+++ NumberTester.java
@@ -10,5 +10,42 @@ package net.htlgrieskirchen.pos3B.lstranzinger18.LambdasHausuebung;
  * @author lukas
  */
 public class NumberTester {
-    
+
+    public interface NumberTest {
+
+        boolean testNumber(int number);
+
+    }
+    NumberTest oddTester;
+    NumberTest primeTester;
+    NumberTest palindromeTester;
+    String fileName;
+    public NumberTester(String fileName) {
+        
+        this.fileName = fileName;
+        
+        
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+        this.oddTester = 
+
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+
+    }
+     public void setPalindrom(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+
+    }
+
+    public void testFile() {
+
+        
+        
+    }
+
 }
