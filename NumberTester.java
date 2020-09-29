137e053a1b2ce5a904201ff296db2148f1b30cb5
LukasStranzes <lukas.stranzinger@outlook.at>
2020-09-29 11:26:43 +0200

    Grundgerüst der Klasse aufgebaut

---
+++ NumberTester.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htlgrieskirchen.pos3B.lstranzinger18.LambdasHausuebung;
+
+/**
+ *
+ * @author lukas
+ */
+public class NumberTester {
+    
+}
