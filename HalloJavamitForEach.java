8d9241411881ef1647e119fce070b71ca8d79409
LukasStranzes <lukas.stranzinger@outlook.at>
2020-09-29 10:53:16 +0200

    Strings der Liste mit ForEach-Loop ausgeben

---
+++ HalloJavamitForEach.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htlgrieskirchen.pos3B.lstranzinger18.LambdasHausuebung;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author lukas
+ */
+public class HalloJavamitForEach {
+    
+    public static void main(String[] args) {
+    
+        List<String> arrList = new ArrayList<>();
+    arrList.add("Nummer 1");        
+    arrList.add("Nummer 2");        
+    arrList.add("Nummer 3");        
+    arrList.add("Nummer 4");        
+        
+        
+        for (String string : arrList) {
+            System.out.println(string);
+            
+            
+            
+            
+        }
+   
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
