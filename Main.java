74eb26ae3eae45be2582812fb9440786fed98db1
LukasStranzes <lukas.stranzinger@outlook.at>
2020-10-04 16:06:40 +0200

    Fehler bei den Lambdas ausfindig gemacht und ausgebessert und TestFile methode fertiggestellt

--- Main.java
+++ Main.java
@@ -3,19 +3,95 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package net.htlgrieskirchen.pos3B.lstranzinger18;
+package net.htlgrieskirchen.pos3B.lstranzinger18.LambdasHausuebung;
+
+import java.io.IOException;
 
 /**
  *
  * @author lukas
  */
-public class main {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+
+        NumberTester ntester = new NumberTester("C:\\Users\\lukas\\Desktop\\NetbeansProjets\\Hausuebung2Lambdas\\src\\net\\htlgrieskirchen\\pos3B\\lstranzinger18\\LambdasHausuebung\\ToBeTested.txt");
+        ntester.setOddEvenTester(x -> {
+
+            boolean result = false;
+
+            if (x % 2 == 0) {
+                result = true;
+                System.out.println("EVEN");
+            } else {
+                System.out.println("ODD");
+            }
+
+            return result;
+        });
+        ntester.setPrimeTester(x
+                -> {
+            boolean result = false;
+            
+            if(x == 1)
+            {
+            result = true;
+                
+            }
+            else
+            {
+            for (int i = 2; i <= x/2; i++) {
+                if(x % i == 0)
+                {
+                
+                result = true;
+                break;
+                }
+                    
+                
+            }
+            }
+            
+            
+            
+            if (result == true) {
+                System.out.println("NO PRIME");
+            } else if (result == false) {
+                System.out.println("PRIME");
+            }
+            return !result;
+
+        });
+
+        ntester.setPalindrom(x -> {
+
+            boolean result = false;
+            int temp = 0;
+            int originalInt = 0;
+            int reversedInt = 0;
+
+            originalInt = x;
+
+            while (x != 0) {
+                temp = x % 10;
+                reversedInt = reversedInt * 10 + temp;
+                x = x / 10;
+
+            }
+
+            if (originalInt == reversedInt) {
+                result = true;
+                System.out.println("PALINDROME");
+            } else {
+                result = false;
+                System.out.println("NO PALINDROME");
+            }
+            return result;
+        });
+        ntester.testFile();
     }
-    
+
 }
--- NumberTester.java
+++ NumberTester.java
@@ -5,6 +5,13 @@
  */
 package net.htlgrieskirchen.pos3B.lstranzinger18.LambdasHausuebung;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author lukas
@@ -19,17 +26,16 @@ public class NumberTester {
     NumberTest oddTester;
     NumberTest primeTester;
     NumberTest palindromeTester;
-    String fileName;
+    public String fileName;
+
     public NumberTester(String fileName) {
-        
+
         this.fileName = fileName;
-        
-        
+
     }
 
     public void setOddEvenTester(NumberTest oddTester) {
         this.oddTester = oddTester;
-       
 
     }
 
@@ -37,16 +43,61 @@ public class NumberTester {
         this.primeTester = primeTester;
 
     }
-     public void setPalindrom(NumberTest palindromeTester) {
+
+    public void setPalindrom(NumberTest palindromeTester) {
         this.palindromeTester = palindromeTester;
 
     }
 
-    public void testFile() {
+    public void testFile() throws FileNotFoundException {
+        int anzahlTestFälle;
 
-      
-        
+        File file = new File(this.fileName);
+        System.out.println(file.exists());
+
+        Scanner scan = null;
+        try {
+            scan = new Scanner(file);
+        } catch (FileNotFoundException e) {
+
+        }
+        anzahlTestFälle = Integer.parseInt(scan.nextLine());
+
+        for (int i = 0; i < anzahlTestFälle; i++) {
+
+            String[] numbersPerLine = (scan.nextLine().split(" "));
+
+            if (numbersPerLine.length > 1) {
+                int chooseTest = Integer.parseInt(numbersPerLine[0]);
+                int toTest = Integer.parseInt(numbersPerLine[1]);
+
+                if(chooseTest != 0 && chooseTest < 4)
+                {
+                switch (chooseTest) {
+
+                    case 1:
+                        oddTester.testNumber(toTest);
+                        break;
+
+                    case 2:
+                        primeTester.testNumber(toTest);
+                        break;
+                    case 3:
+                        palindromeTester.testNumber(toTest);
+                        break;
+
+                }
+                }
+                else{
+                
+                    System.out.println("ES GIBT KEINEN ZU DURCHFÜHRENDEN TEST MIT DIESEM INDEX");
+                }
+            }
+
+        }
+
+        scan.close();
+
     }
-   
 
 }
