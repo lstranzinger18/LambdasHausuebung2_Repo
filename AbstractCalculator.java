8d195ea8d6c9681a3fa5ed80a532c1ef2b1d195a
LukasStranzes <lukas.stranzinger@outlook.at>
2020-10-04 19:33:11 +0200

    Vererbte Methoden implementiert und Fehler ausgebessert

--- AbstractCalculator.java
+++ AbstractCalculator.java
@@ -5,10 +5,32 @@
  */
 package net.htlgrieskirchen.pos3B.lstranzinger18.LambdasHausuebung;
 
+import net.htlgrieskirchen.pos3B.lstranzinger18.LambdasHausuebung.Number.CalculationOperation;
+
 /**
  *
  * @author lukas
  */
-public class AbstractCalculator {
-    
+public abstract class AbstractCalculator {
+
+    protected CalculationOperation add;
+    protected CalculationOperation subtract;
+    protected CalculationOperation mutliply;
+    protected CalculationOperation divide;
+
+    public AbstractCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation mutliply, CalculationOperation divide) {
+        this.add = add;
+        this.subtract = subtract;
+        this.mutliply = mutliply;
+        this.divide = divide;
+
+    }
+
+    public abstract Number add(Number a, Number b);
+
+    public abstract Number subtract(Number a, Number b);
+
+    public abstract Number multiply(Number a, Number b);
+
+    public abstract Number divide(Number a, Number b);
 }
